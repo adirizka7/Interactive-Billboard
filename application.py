8ef18d00fa9536765b7d2cbf7aba14e5cf9a46eb
Adi Rizka <adi.rizka@tokopedia.com>
2019-10-22 23:40:05 +0700

    add speech to text and integrate with kata

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-import os
+import requests, os
+from flask import Flask, render_template, request
+from config import cfg
 
 PEOPLE_FOLDER = os.path.join('static', 'Photos')
 
@@ -10,6 +11,18 @@ app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 def show_index(image):
     full_filename = os.path.join(app.config['UPLOAD_FOLDER'], image)
     return render_template("index.html", user_image = full_filename)
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    text = request.form['q']
+
+    s = requests.post(
+            "https://geist.kata.ai/nlus/adirizka7:Billboard/predict",
+            data={"text": text},
+            headers={"Authorization":"Bearer " + cfg['bearer']}
+            )
+
+    return s.text
 
 if __name__ == '__main__':
     app.run(debug = True)
