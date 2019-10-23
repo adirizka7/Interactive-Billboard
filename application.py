e5ec91d4ebdcfefb6bf72e0845bde837a9f03ea7
Adi Rizka <adi.rizka@tokopedia.com>
2019-10-23 23:58:15 +0700

    map intention to images

--- application.py
+++ application.py
@@ -1,8 +1,12 @@
-import requests, os
+import requests
+import os
+import json
+import logging
 from flask import Flask, render_template, request
 from config import cfg
 
 PEOPLE_FOLDER = os.path.join('static', 'Photos')
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
@@ -16,13 +20,19 @@ def show_index(image):
 def handle_data():
     text = request.form['q']
 
+    logging.info('Text Input : {}'.format(text))
     s = requests.post(
             "https://geist.kata.ai/nlus/adirizka7:Billboard/predict",
             data={"text": text},
             headers={"Authorization":"Bearer " + cfg['bearer']}
             )
 
-    return s.text
+    logging.info('Response received : {}'.format(json.dumps(json.loads(s.text), indent=4)))
+    s = json.loads(s.text)['result']
+    intent = [x for x in s if s[x] != []]
+    logging.info('Intent extracted : {}'.format(intent))
+
+    return show_index(intent[0] + '/1.png') if len(intent) else show_index('Mulai.png')
 
 if __name__ == '__main__':
     app.run(debug = True)
