62a413a0ac9a3cd98fb8084f91af980e933b0a89
Adi Rizka <adi.rizka@tokopedia.com>
2019-10-25 11:03:52 +0700

    change photo name

--- application.py
+++ application.py
@@ -35,4 +35,4 @@ def handle_data():
     return show_index(intent[0] + '/1.png') if len(intent) else show_index('Mulai.png')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True, host='0.0.0.0')
